70383b8d38c6ace93fb179d0fa9a7d6126674354
Win_9 <krrr883@gmail.com>
2022-06-20 23:04:11 +0900

    table, object Mapping

---
+++ src/main/java/com/example/ex1hellojpa/Ex1HelloJpaApplication.java
@@ -0,0 +1,37 @@
+package com.example.ex1hellojpa;
+
+import com.example.ex1hellojpa.domain.Member;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+@SpringBootApplication
+public class Ex1HelloJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex1HelloJpaApplication.class, args);
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+
+}
